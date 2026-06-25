6897d7d1ca9b807f50b29e59ce95cd8acbc885d2
ashish0jha <ashish2893jnv@gmail.com>
2026-06-25 13:07:49 +0530

    Stores the chats in data base

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,4 +1,5 @@
 const socket = require("socket.io")
+const Chat = require("../models/chat")
 
 const initializeSocket = (server) => {
     const io = socket(server, {
@@ -10,16 +11,35 @@ const initializeSocket = (server) => {
     io.on("connection", (socket) => {
         socket.on("joinChat", ({ firstName, userId, targetUserId }) => {
             const roomId = [userId, targetUserId].sort().join("_");
+            console.log(firstName+" Joined the Room : "+roomId);
             socket.join(roomId);
         })
-        socket.on("sendMessage", ({
-            firstName,
-            userId,
-            targetUserId,
-            text,
-        }) => {
-            const roomId = [userId, targetUserId].sort().join("_");
-            io.to(roomId).emit("messageReceived",{firstName,text});
+        socket.on("sendMessage",async ({firstName,lastName,userId,targetUserId,text}) => {
+            try{
+                const roomId = [userId, targetUserId].sort().join("_");
+
+                let chat = await Chat.findOne({
+                    participants:{$all : [userId,targetUserId]}
+                })
+
+                if(!chat){
+                    chat = new Chat({
+                        participants:[userId,targetUserId],
+                        messages:[]
+                    })
+                }
+
+                chat.messages.push({
+                    senderId:userId,
+                    text,
+                })
+
+                await chat.save();
+                io.to(roomId).emit("messageReceived",{userId,firstName,lastName,text});
+            }
+            catch(err){
+                console.error("ERROR : "+err.message);
+            }
         })
         socket.on("disconnect", () => { })
     })
